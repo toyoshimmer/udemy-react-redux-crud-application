2375196a548aa6948b1830d88b5ab751c404be07
toyoshimmer <toyoshimmer@gmail.com>
2018-06-30 12:04:32 +0900

    implement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const increment = () => ({
+    type: DECREMENT
+})
